f262c505aab98cfc94514b19f7399a2615967b20
btpjamal <jvbraga2608@gmail.com>
2026-08-12 23:37:42 -0300

    mudei o profile para "test" para testar coisas

--- src/main/java/dev/jamal/projetotcc/config/DevDataSeeder.java
+++ src/main/java/dev/jamal/projetotcc/config/DevDataSeeder.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+@Profile("test")
 @RequiredArgsConstructor
 public class DevDataSeeder {
 
