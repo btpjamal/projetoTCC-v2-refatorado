5bb7b26de8ffe91008e8b9599a6e2b1d196c718a
btpjamal <joaothexkronax@hotmail.com>
2026-08-25 14:22:43 -0300

    listagem de feedback por userId ao repository

--- src/main/java/dev/jamal/projetotcc/Repository/UserRecommendationFeedbackRepository.java
+++ src/main/java/dev/jamal/projetotcc/Repository/UserRecommendationFeedbackRepository.java
@@ -1,6 +1,7 @@
 package dev.jamal.projetotcc.Repository;
 
 import dev.jamal.projetotcc.Entities.UserRecommendationFeedback;
+import dev.jamal.projetotcc.Enum.RecommendationFeedbackType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -15,4 +16,11 @@ public interface UserRecommendationFeedbackRepository
     );
 
     List<UserRecommendationFeedback> findByUserId(Long userId);
+
+    List<UserRecommendationFeedback> findByUser_IdAndTipo(
+            Long userId,
+            RecommendationFeedbackType tipo
+    );
+
+    List<UserRecommendationFeedback> findByUser_Id(Long userId);
 }
