2be531740a783ec712725f96b541ef3c264c66ef
btpjamal <joaothexkronax@hotmail.com>
2026-08-27 12:53:08 -0300

    só altera o nome do arquivo

--- src/main/java/dev/jamal/projetotcc/DTO/UserHobby/UserHobbyResponseDTO.java
+++ src/main/java/dev/jamal/projetotcc/DTO/UserHobby/UserHobbyResponseDTO.java
@@ -2,7 +2,7 @@ package dev.jamal.projetotcc.DTO.UserHobby;
 
 import dev.jamal.projetotcc.Enum.NivelExperiencia;
 
-public record UserHobbyLevelResponseDTO(
+public record UserHobbyResponseDTO(
         Long hobbyId,
         String nome,
         NivelExperiencia nivelAtual
